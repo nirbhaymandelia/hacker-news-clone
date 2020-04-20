11f28fe392500a2717881b7c42bd5b21c3a5daf8
Nirbhay Kr Mandelia <nmandelia@sapient.com>
2020-04-20 16:58:17 +0530

    fix startup script

--- scripts/serve.js
+++ scripts/serve.js
@@ -1,17 +1,12 @@
-/* eslint-disable consistent-return */
-/* eslint-disable no-console */
-
-// eslint-disable-next-line import/no-unresolved
+/* eslint-disable consistent-return, no-console, import/no-unresolved */
 const app = require('../dist/server/main.js').default;
 
 const PORT = process.env.PORT || 3000;
 
 // Use the native Node.js cluster module to create a worker processes for each CPU
-app.setup();
 app.listen(PORT, (err) => {
   if (err) {
     return console.error(err);
   }
-
   console.info(`Server running on port ${PORT}`);
 });
